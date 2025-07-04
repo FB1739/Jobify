b32c5c2acd2f909e025937df1f83584679fd902b
ferchu <ferdmc@gmail.com>
2025-07-04 00:01:25 -0300

    adding, model, controller and modifying the server

--- server.js
+++ server.js
@@ -1,5 +1,47 @@
-import { value } from "./test-named.js";
-import randomValue from "./test-default.js"
+import * as dotenv from 'dotenv';
+import express from 'express';
+import morgan from 'morgan';
+import jobRouter from './routers/jobRouter.js';
+import mongoose from 'mongoose';
+import 'express-async-errors';
 
-console.log(value);
-console.log(randomValue);
+const app = express();
+dotenv.config();
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.post('/', (req, res) => {
+  console.log(req);
+
+  res.json({ message: 'Data received', data: req.body });
+});
+
+app.use('/api/v1/jobs', jobRouter);
+
+app.use('*', (req, res) => {
+  res.status(404).json({ msg: 'not found' });
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({ msg: 'something went wrong' });
+});
+
+const port = process.env.PORT || 5100;
+
+try {
+  await mongoose.connect(process.env.MONGO_URL);
+  app.listen(port, () => {
+    console.log(`server running on PORT ${port}....`);
+  });
+} catch (error) {
+  console.log(error);
+  process.exit(1);
+}
